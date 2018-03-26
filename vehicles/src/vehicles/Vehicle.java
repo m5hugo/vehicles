9617cf8a57158a9904d4d77f6cab632e95971386
hugo <sinacabar@gmail.com>
2018-03-26 23:23:34 +0200

    Afegir mètodes 'pujarMarxa' i 'baixaMarxa'

--- vehicles/src/vehicles/Vehicle.java
+++ vehicles/src/vehicles/Vehicle.java
@@ -3,13 +3,23 @@ package vehicles;
 public class Vehicle {
 	
 	private static final int velocitat_maxima = 120;
+	private static final int marxes = 5;
       
 	private double cilindrada;
 	private int portes;
 	private int velocitat_actual;
+	private int marxaActual;
 	
 	public Vehicle(){
 		this.velocitat_actual = 0;
+	}
+	
+	public void pujaMarxa(){
+		if (this.marxaActual < marxes) this.marxaActual += 1;
+	}
+	
+	public void baixaMarxa(){
+		if (this.marxaActual > 0) this.marxaActual -= 1;
 	}
 	
 	public void accelerar(){		
